097405a2e13f3a68d3952b05aa4d3423674bcb4f
Leena Loo <35936311+leenaloo@users.noreply.github.com>
2020-04-27 15:02:20 -0400

    added view controller files for welcome, sign up and log in views

---
+++ Circle/Circle/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  Circle
+//
+//  Created by Leena Loo on 4/27/20.
+//  Copyright © 2020 Ivanna Peña and Leena Loo. All rights reserved.
+//
+//this is the view controller code for the initial welcome page view where it prompts the user to sign up or log in
+
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
