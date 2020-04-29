e61b66f5eb7a1ca58e0a3cad73117412bd7a18a0
Leena Loo <35936311+leenaloo@users.noreply.github.com>
2020-04-29 01:15:39 -0400

    started segmented control code for a single group and changed some storyboards

--- Circle/Circle/singleGroupViewController.swift
+++ Circle/Circle/singleGroupViewController.swift
@@ -10,13 +10,32 @@ import UIKit
 
 class singleGroupViewController: UIViewController {
 
+    @IBOutlet weak var singleGroupNavBar: UISegmentedControl!
+    @IBOutlet weak var membersTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func singleGroupSegmentControl(_ sender: Any) {
+        if(singleGroupNavBar.selectedSegmentIndex == 0){
+            //show only posts for this group
+            //hide the table view of members
+            //might need to do sublayers here with this view controller as the parent
+            membersTableView.isHidden = true;
+        }
+        if(singleGroupNavBar.selectedSegmentIndex == 1){
+            //show table view of members in this group
+            membersTableView.isHidden = false;
+        }
+        if(singleGroupNavBar.selectedSegmentIndex == 2){
+            //show about info for this group
+            membersTableView.isHidden = true;
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
