1b393870ed6977a50f31c7483e088327f49dfb58
Leena Loo <35936311+leenaloo@users.noreply.github.com>
2020-05-01 16:42:10 -0400

    changed to tab view controller, added messages screen, opening animation screen

---
+++ Circle/Circle/initialLoadingAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  initialLoadingAnimation.swift
+//  Circle
+//
+//  Created by Leena Loo on 5/1/20.
+//  Copyright © 2020 Ivanna Peña and Leena Loo. All rights reserved.
+//
+
+import UIKit
+
+class initialLoadingAnimation: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Circle/Circle/messagingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  messagingViewController.swift
+//  Circle
+//
+//  Created by Leena Loo on 5/1/20.
+//  Copyright © 2020 Ivanna Peña and Leena Loo. All rights reserved.
+//
+
+import UIKit
+
+class messagingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
