165d88156008870fd6191fe423198fba3383a22f
Leena Loo <35936311+leenaloo@users.noreply.github.com>
2020-05-03 17:00:10 -0400

    trying to do the group page but i disconnected it from the flow because it keeps crashing, i hope this didnt break anything else

---
+++ Circle/Circle/groupViews/groupPostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  groupPostViewController.swift
+//  Circle
+//
+//  Created by Leena Loo on 5/3/20.
+//  Copyright © 2020 Ivanna Peña and Leena Loo. All rights reserved.
+//
+
+import UIKit
+
+class groupPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Circle/Circle/singleGroupViewController.swift
+++ Circle/Circle/singleGroupViewController.swift
@@ -10,29 +10,70 @@ import UIKit
 
 class singleGroupViewController: UIViewController {
 
-    @IBOutlet weak var singleGroupNavBar: UISegmentedControl!
-    @IBOutlet weak var membersTableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        /*
+        let groupPostsView = storyboard?.instantiateViewController(withIdentifier: "groupPosts") as! groupPostViewController
+        let groupMembersView = storyboard?.instantiateViewController(withIdentifier: "groupMembers") as! groupMembersViewController
+        let groupAboutView = storyboard?.instantiateViewController(withIdentifier: "groupAbout") as! groupAboutViewController
+
+        
+        //mainTabController.modalPresentationStyle = .fullScreen
+        //present(mainTabController, animated: false, completion: nil)
+        /*let mainTabController = storyboard?.instantiateViewController(withIdentifier: "MainTabController") as! MainTabController
+        mainTabController.modalPresentationStyle = .fullScreen
+        present(mainTabController, animated: false, completion: nil)
+
+        /*
+        viewContainer.addSubview(groupPostsView)
+        viewContainer.addSubview(groupMembersView)
+        viewContainer.addSubview(groupAboutView)
+
+        
+    }
+    func displayContentPosts(_ sender: UIViewController){
+
+        addChild(groupPostsView)
+        viewContainer.addSubview(groupPostViewController().view)
+        didMove(toParent: self)
+    
+    }
+    func displayContentMembers(_ sender: UIViewController){
+
+
+        addChild(groupMembersView)
+        viewContainer.addSubview(groupMembersViewController().view)
+        didMove(toParent: self)
+    
+    }
+    func displayContentAbout(_ sender: UIViewController){
+        addChild(groupAboutView)
+        viewContainer.addSubview(groupAboutViewController().view)
+        didMove(toParent: self)
+         
+         
+
+    
+}*/*/
     }
     
-    @IBAction func singleGroupSegmentControl(_ sender: Any) {
-        if(singleGroupNavBar.selectedSegmentIndex == 0){
-            //show only posts for this group
-            //hide the table view of members
-            //might need to do sublayers here with this view controller as the parent
-            membersTableView.isHidden = true;
-        }
-        if(singleGroupNavBar.selectedSegmentIndex == 1){
-            //show table view of members in this group
-            membersTableView.isHidden = false;
-        }
-        if(singleGroupNavBar.selectedSegmentIndex == 2){
-            //show about info for this group
-            membersTableView.isHidden = true;
+
+        func groupSegControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        
+        case 0:
+            present(groupPostsView, animated: false, completion: nil)
+            break
+        case 1:
+            present(groupMembersView, animated: false, completion: nil)
+            break
+        case 2:
+                        present(groupAboutView, animated: false, completion: nil)
+            break
+        default:
+            break
         }
     }
     
@@ -44,6 +85,8 @@ class singleGroupViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    */*/
 
+
+}
 }
