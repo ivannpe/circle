a6fe5ad6405526ae988d7b43f7cbc3286c62c6b9
Ivanna <ivannpe@gmail.com>
2020-05-03 21:50:10 -0400

    Adding Group Cells Info
    connecting group story board, and adding group cell info

--- Circle/Circle/groupsViewController.swift
+++ Circle/Circle/groupsViewController.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class groupsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var groupsTableView: UITableView!
+    var groupsArray = [Group]()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            groupsTableView.delegate = self
+            groupsTableView.dataSource = self
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+                DataService.instance.getAllGroups { (returnedGroupsArray) in
+                    self.groupsArray = returnedGroupsArray
+                    self.groupsTableView.reloadData()
+                }
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    extension groupsViewController: UITableViewDelegate, UITableViewDataSource {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return groupsArray.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell") as? GroupVCCell else { return UITableViewCell() }
+            let group = groupsArray[indexPath.row]
+            cell.configureCell(title: group.groupTitle, description: group.groupDesc)
+            return cell
+        }
+        //ADD ONCE FEED IS MADE
+//        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//            guard let groupFeedVC = storyboard?.instantiateViewController(withIdentifier: "GroupFeedVC") as? GroupFeedVC else { return }
+//            groupFeedVC.initData(forGroup: groupsArray[indexPath.row])
+//            presentDetail(groupFeedVC)
+//        }
     }
-    */
-
-}
